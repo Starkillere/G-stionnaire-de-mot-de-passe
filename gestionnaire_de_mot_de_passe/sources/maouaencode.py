ec1c8dbb3fa404acf7e2f78a6dfb5fec4e1af2e1
Starkiller <83246502+Starkillere@users.noreply.github.com>
2022-03-09 15:34:47 +0100

    Update maouaencode.py

--- gestionnaire_de_mot_de_passe/sources/maouaencode.py
+++ gestionnaire_de_mot_de_passe/sources/maouaencode.py
@@ -1,8 +1,9 @@
 #-*- coding:utf-8 -*-
 
+#-*- coding:utf-8 -*-
+
 import os
 import sys
-import math
 import random
 from operator import xor
 
@@ -14,7 +15,7 @@ class CodageMoua:
     """
 
     def __init__(self):
-        self.element = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+        self.elements = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
                         'o', 'p', 'q', 'r', 's', 't', 'u',
                         'h', 'i', 'j', 'k', 'l', 'm', 'n',
                         'v', 'w', 'x', 'y', 'z', 'A', 'B',
@@ -28,33 +29,44 @@ class CodageMoua:
                         '"', '\'', '{', '}', '|', '`', '°',
                         '[', ']',  '+', '.', '=', '?', ";", 
                         ':', '!', '§', '¤', '€', '£', '¨', 
-                        '<', '>']
+                        '<',  '>', 'Σ', '~']
     
-    def __algorithme_fermat(self, valeur:int) -> int :
+    def __algorithme(self, valeur:int) -> int :
         assert type(valeur) == int
-        res = 2**2**valeur+1
+        res = -valeur + len(self.elements)
         return res
     
-    def __inv__algorithme_fermat(self, valeur:int) -> int :
+    def __inv_algorithme(self, valeur:int) -> int :
         assert type(valeur) == int
-        res = round(math.log((math.log(2**2**valeur+1)-1)/math.log(2))/math.log(2), 0)
+        res = abs(valeur - len(self.elements))
         return res
 
-    def master_key_genrator(self,  master_pasword:str) -> str :
-        assert type(master_pasword) == str
+    def master_key_genrator(self) -> str :
         bytes = os.urandom(32)
         csprng = random.SystemRandom()
-        random_int = csprng.randint(0, sys.maxsize)   
+        random_int = [self.elements[csprng.randint(0, sys.maxsize)%len(self.elements)] for i in range(32)]
+        master_key = ''.join(random_int)
+        return master_key
                         
-    def encode(self, master_key:str, password:str) -> str :
-        assert type(master_key) == str
-        assert type(password) == str 
-        if len(password) % 19 != 0:
-            pass
-
-    def decode(self, master_key:str,  password:str) -> str :
+    def encode(self, password:str, master_key:str) -> str :
         assert type(master_key) == str
         assert type(password) == str
+        if len(master_key) > len(password):
+            password += 'Σ'+''.join([self.elements[random.SystemRandom().randint(0, sys.maxsize)%len(self.elements)] for i in range((len(master_key) - len(password))-1)])
+        valeurs_master_key = [self.elements.index(value) for value in master_key]
+        valeurs_password = [self.elements.index(value) for value in password]
+        res_algo_password = [self.__algorithme(value)%len(self.elements) for value in valeurs_password]
+        chiffre = [(valeurs_master_key[i] +  res_algo_password[i])%len(self.elements) for i in range(len(valeurs_master_key))]
+        return ''.join([self.elements[value] for value in chiffre])
+
+    def decode(self, chiffre:str, master_key:str) -> str :
+        assert type(master_key) == str
+        assert type(chiffre) == str
+        valeurs_master_key = [self.elements.index(value) for value in master_key]
+        valeurs_chiffre = [self.elements.index(value) for value in chiffre]
+        res_inv_algo_password = [(valeurs_chiffre[i] - valeurs_master_key[i])%len(self.elements) for i in range(len(valeurs_master_key))]
+        clair =  [self.__inv_algorithme(value)%len(self.elements) for value in res_inv_algo_password]
+        return (''.join([self.elements[value] for value in clair]).split('Σ'))[0]
 
 if __name__ == '__main__':
-    my_pass = CodageMoua()
+    pass
