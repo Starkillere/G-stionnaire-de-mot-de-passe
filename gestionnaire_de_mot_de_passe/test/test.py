033ae6fe3b0c2429120e3dd6ea7bb82e2f7c5590
Starkiller <83246502+Starkillere@users.noreply.github.com>
2022-03-19 20:57:33 +0100

    Add files via upload

---
+++ gestionnaire_de_mot_de_passe/test/test.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 -*-
+import unitest
+
+class test(unitest):
+    def __init__(self) -> None:
+        if self.a == 2:
+            print("ok")
+        
+    def tester(self, nob):
+        self.a =  nob
